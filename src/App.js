40136e5d40a8384ef5f7da424986ba1b76093c02
JvitorJ <joaovictorrsende59@gmail.com>
2023-05-12 22:04:55 -0300

    commit 01

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Sobre from "./components/Sobre";
+import Portifolio from './components/Portifolio';
+import Contatos from './components/Contatos';
+import NotFound from './components/NotFound';
+import PageLayout from './layouts/PageLayout';
+import Produtos from './components/Produtos';
+
+const App = () => {
+  return (
+    <>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<PageLayout />}>
+              <Route index element={<Dashboard/>}/>
+              <Route path='/sobre' element={<Sobre />} />
+              <Route path='/portifolio' element={<Portifolio />} />
+              <Route path='/contatos' element={<Contatos />} />
+              <Route path='/produtos' element={<Produtos />} />☻
+            </Route>
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './style.css'
+
+const Header = () => {
+    let logado = true;
+    let nome = 'Gabriel';
+
+    return(
+        <>
+            <header>
+                <nav>
+                    <div className='logo'>
+                        LOGO
+                    </div>
+                    {logado && 
+                        <div className="perfil">
+                            <h5>Bem vindo(a) {nome}!</h5>
+                        </div>
+                    }
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        {logado && 
+                         <li>
+                            <a href="/sobre">Sobre</a>
+                         </li>
+                        }
+                        <li>
+                            <a href="/portifolio">Portifólio</a>
+                        </li>
+                        <li>
+                            <a href="/contatos">Contatos</a>
+                        </li>
+                        <li>
+                            <a href="/produtos">Produtos</a>
+                        </li>
+                    </ul>
+                </nav>
+                <h6>&copy; Todos os direitos reservados.</h6>
+            </header>
+        </>
+    );
+}
+
+export default Header;
